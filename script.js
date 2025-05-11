543f79f49777c331d46b98bfb22047d17d131b4d
JoseLuisTF29 <160427812+JoseLuisTF29@users.noreply.github.com>
2025-05-11 14:25:09 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $(".valentines-day").click(function () {
+      $('#card').css({
+        'visibility': 'visible',
+        'transform': 'scale(1)',
+        'opacity': 1
+      });
+    });
+  });
+  
